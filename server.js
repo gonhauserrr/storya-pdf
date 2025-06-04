9b7fc7680d6d11f2457ae829d3aa1225a0aec672
goncheers <gonzalo@cheerscontracts.com>
2025-06-04 15:38:41 -0300

    asd
    asd

--- server.js
+++ server.js
@@ -927,7 +927,7 @@ stream.on('finish', () => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
